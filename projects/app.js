5cfa55ec12bb3d75e88e2d3c0e2c1f16ef1e77d8
tocology <hwangjun7777@gmail.com>
2015-07-30 23:45:32 +0900

    first

--- projects/app.js
+++ projects/app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+var routes = require('./routes');
+var errorHandlers = require('./middleware/errorhandlers');
+var log = require('./middleware/log');
+app.use(log.logger);
+app.use(express.static(__dirname + '/static'));
 
-app.get('*', function(req,res){
-    res.send('Express Response');
-});
+app.get('/', routes.index);
+app.get('/login', routes.login);
+app.post('/login', routes.loginProcess);
+app.get('/chat', routes.chat);
+app.use(errorHandlers.notFound);
 
 app.listen(3000);
 console.log("App server running on port 3000")
