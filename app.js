16f907e4a72542a14a20a59b4390c9f0f7757158
tocology <hwangjun7777@gmail.com>
2015-08-08 09:28:43 +0900

    resetting

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/', routes.index);
 app.get('/login', routes.login);
 app.post('/login', routes.loginProcess);
 app.get('/chat', routes.chat);
+app.get('/error', function(req, res, next){
+  next(new Error('A contrived error'));
+});
+
+app.use(errorHandlers.error);
 app.use(errorHandlers.notFound);
 
 app.listen(3000);
