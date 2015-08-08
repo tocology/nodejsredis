8899c1e68b687590cff77c2c5be8f782511fb3a9
tocology <hwangjun7777@gmail.com>
2015-08-09 00:13:59 +0900

    8/8

--- app.js
+++ app.js
@@ -3,13 +3,47 @@ var app = express();
 var routes = require('./routes');
 var errorHandlers = require('./middleware/errorhandlers');
 var log = require('./middleware/log');
+var partials = require('express-partials');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var bodyParser = require('body-parser');
+var csrf = require('csurf');
+var util = require('./middleware/utilities');
+
+app.set('view engine', 'ejs');
 app.use(log.logger);
 app.use(express.static(__dirname + '/static'));
+app.use(partials());
+app.set('view options', {defaultLayout: 'layout'});
+app.use(cookieParser('secret'));
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+  store: new RedisStore(
+    {url: 'redis://localhost'})
+  })
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(csrf());
+app.use(util.csrf);
+app.use(util.authenticated);
+
+app.use(function(req, res, next){
+  if(req.session.pageCount)
+    req.session.pageCount++;
+  else
+    req.session.pageCount = 1;
+   next();
+});
 
 app.get('/', routes.index);
 app.get('/login', routes.login);
 app.post('/login', routes.loginProcess);
-app.get('/chat', routes.chat);
+app.get('/chat', [util.requireAuthentication], routes.chat);
+app.get('/logout', routes.logOut);
 app.get('/error', function(req, res, next){
   next(new Error('A contrived error'));
 });
--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,29 @@ module.exports.index = index;
 module.exports.login = login;
 module.exports.loginProcess = loginProcess;
 module.exports.chat = chat;
+module.exports.logOut = logOut;
+
+var util = require('../middleware/utilities');
 
 function index(req, res){
-  res.send('Index');
+  res.cookie('IndexCookie', 'This was set from Index');
+  res.render('index', {title: 'Index'});
 };
 function login(req, res){
-  res.send('Login');
+  res.render('login', {title: 'Login'});
 };
 function loginProcess(req, res){
+  var isAuth = util.auth(req.body.username, req.body.password, req.session);
+  if(isAuth) {
+    res.redirect('/chat');
+  } else {
+    res.redirect('/login');
+  }
+};
+function logOut(req, res){
+  util.logOut(req.session);
   res.redirect('/');
 };
 function chat(req, res){
-  res.send('Chat');
+  res.render('chat', {title: 'Chat'});
 };
